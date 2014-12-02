e62c08905dc00688ae90eb4ce311f4f55eb9c980
Idar Borlaug <idar.borlaug@gmail.com>
2014-12-02 13:18:23 +0100

    #167 logging av steg

--- svarut-sak-import/src/main/java/no/ks/svarut/sakimport/Main.java
+++ svarut-sak-import/src/main/java/no/ks/svarut/sakimport/Main.java
@@ -1,5 +1,6 @@
 package no.ks.svarut.sakimport;
 
+import no.geointegrasjon.rep.arkiv.dokument.xml_schema._2012_01.Dokument;
 import no.geointegrasjon.rep.arkiv.kjerne.xml_schema._2012_01.Journalpost;
 import no.ks.svarut.sakimport.GI.Saksimporter;
 
@@ -20,8 +21,11 @@ public class Main {
             final Fil fil = nedlaster.hentForsendelseFil(forsendelse);
 
             final Journalpost journalpost = importer.importerJournalPost(forsendelse);
-            importer.importerDokument(journalpost,forsendelse.getTittel(), fil.getFilename(), fil.getMimetype(), fil.getBytes(), true);
+            System.out.println("Journalpost " + journalpost.getJournalpostnummer() + " laget for forsendelse " + forsendelse.getTittel());
+            final Dokument dokument = importer.importerDokument(journalpost, forsendelse.getTittel(), fil.getFilename(), fil.getMimetype(), fil.getBytes(), true);
+            System.out.println("Dokument " + dokument.getDokumentnummer() + " laget for forsendelse " + forsendelse.getTittel());
             nedlaster.kvitterForsendelse(forsendelse);
+            System.out.println("Forsendelse " + forsendelse.getTittel() + " ferdig mottat.");
         }
     }
 }
