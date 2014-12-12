09d419d3ac74f3a2888ac815d5f4b2fbd5839c47
Idar Borlaug <idar.borlaug@gmail.com>
2014-12-12 13:53:39 +0100

    #167 fikset release script

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+set -e
 setValues() {
     targethost=""
 
@@ -9,7 +9,7 @@ setValues() {
         case $Option in
             v) targethost="vagrant@filarkiv";;
             t) targethost="kssuadmin@kssufilarkiv.usrv.ubergenkom.no";;
-            v) targethost="kssuadmin@kssufilarkiv.srv.bergenkom.no";;
+            p) targethost="kssuadmin@kssufilarkiv.srv.bergenkom.no";;
             ?) usage; exit 0;;
         esac
     done
@@ -38,8 +38,6 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 projectversion=$(git describe --abbrev=0)
 revision=$(git describe --long | sed "s/.*-\(.*\)-.*/\1/")
 mkdir -p target
-
-cp  target/svarut-sak-import-${projectversion}.zip 
 
 scp svarut-sak-import/target/svarut-sak-import-dist.zip ${targethost}:/nfs/svarut-sak-import/releases/svarut-sak-import-${projectversion}.zip
 ssh ${targethost} "(cd /nfs/svarut-sak-import/releases && sudo ln -sf svarut-sak-import-${projectversion}.zip svarut-sak-import-latest.zip)"
