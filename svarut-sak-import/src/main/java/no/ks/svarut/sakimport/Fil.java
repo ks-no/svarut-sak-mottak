fc7bcb508c378e6b25270aaa401e826907c32ab3
Siv Midtun Hollup <siv.hollup@gmail.com>
2015-01-14 14:32:43 +0100

    #170 Dekrypterer vha bytearrays i stedet for strømmer

--- svarut-sak-import/src/main/java/no/ks/svarut/sakimport/Fil.java
+++ svarut-sak-import/src/main/java/no/ks/svarut/sakimport/Fil.java
@@ -2,8 +2,11 @@ package no.ks.svarut.sakimport;
 
 import no.ks.svarut.sakimport.GI.SakImportConfig;
 import no.ks.svarut.sakimport.kryptering.Kryptering;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -11,6 +14,7 @@ public class Fil implements AutoCloseable{
     private final String mimetype;
     private final String filename;
     private final InputStream inputStream;
+    private final byte[] content;
     private HttpEntity entity;
     Kryptering kryptering;
 
@@ -18,6 +22,9 @@ public class Fil implements AutoCloseable{
     public Fil(HttpEntity entity, String mimetype, String filename, SakImportConfig config) throws IOException {
         this.entity = entity;
         this.inputStream = entity.getContent();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        entity.writeTo(outputStream);
+        this.content = outputStream.toByteArray();
         this.mimetype = mimetype;
         this.filename = filename;
         this.kryptering = new Kryptering(config);
@@ -25,12 +32,19 @@ public class Fil implements AutoCloseable{
 
     public Fil(InputStream zis, String mimetype, String filename) {
         inputStream = zis;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            IOUtils.copy(zis, outputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.content = outputStream.toByteArray();
         this.mimetype = mimetype;
         this.filename = filename;
     }
 
     public InputStream getData() throws IOException {
-        return kryptering.dekrypterForSvarUt(inputStream);
+        return new ByteArrayInputStream(kryptering.dekrypterForSvarUt(this.content));
     }
 
     public String getMimetype() {
